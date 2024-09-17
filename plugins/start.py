236a2f5556bc996c57fae65d1c736815a8f504e4
rohit870891 <koyeb77user@gmail.com>
2024-09-17 19:10:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -103,8 +103,9 @@ async def start_command(client: Client, message: Message):
                 return
             await temp_msg.delete()
             snt_msgs = []
+
             for msg in messages:
-               original_caption = msg.caption.html if msg.caption else ""
+                 original_caption = msg.caption.html if msg.caption else ""
                 if CUSTOM_CAPTION:
                     caption = f"{original_caption}\n\n{CUSTOM_CAPTION}"
                 else:
