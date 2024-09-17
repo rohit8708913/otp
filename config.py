041a3ba69b40c4b112946174be1618db38d00ef6
rohit870891 <koyeb77user@gmail.com>
2024-09-17 18:59:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7223199796:AAFPS7v8m3Uqe4d75gzR8ICGI5BP8CF4uiM")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7223199796:AAHIBqFpIlcTKMqf0gg1vybTZgp1UlpbBh0")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "25839862"))
