79184c3cfd4bb119186144b863c7187b254d38ec
rohit8708913 <koyeb67user@gmail.com>
2024-09-21 14:07:31 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,7 +12,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Rohit</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
