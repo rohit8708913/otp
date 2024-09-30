43d5655919d0235a5c30c8eeff600582ce8c23a2
rohit8708913 <koyeb67user@gmail.com>
2024-10-01 04:49:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -206,7 +206,7 @@ async def start_command(client: Client, message: Message):
         try:
             reply_markup = InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("😊 About Me", callback_data="about"), InlineKeyboardButton("🔒 Close", callback_data="close")]
+                    [InlineKeyboardButton("😊 About Me", callback_data="about"), InlineKeyboardButton("🔒 Close", callback_data="close")],
                     [InlineKeyboardButton('BUY PREMIUM', callback_data='buy_prem')]
                 ]
             )
