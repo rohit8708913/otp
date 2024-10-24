485b4744f14e9462c0c581669bd10877281ae3c2
rohit8708913 <koyeb67user@gmail.com>
2024-10-24 17:36:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -225,3 +225,156 @@ async def start_command(client: Client, message: Message):
         except Exception as e:
             print(e)
 
+
+#=====================================================================================##
+
+WAIT_MSG = """"<b>Processing ...</b>"""
+
+REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
+
+#=====================================================================================##
+
+    
+@Bot.on_message(filters.command('start') & filters.private)
+async def not_joined(client: Client, message: Message):
+    buttons = [
+        [
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •", url=client.invitelink2),
+        ]
+    ]
+    try:
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text = 'Try Again',
+                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                )
+            ]
+        )
+    except IndexError:
+        pass
+
+    await message.reply_photo(
+       photo = FORCE_PIC,
+        caption = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+    )
+
+@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
+async def get_users(client: Bot, message: Message):
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    users = await full_userbase()
+    await msg.edit(f"{len(users)} users are using this bot")
+
+@Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
+async def send_text(client: Bot, message: Message):
+    if message.reply_to_message:
+        query = await full_userbase()
+        broadcast_msg = message.reply_to_message
+        total = 0
+        successful = 0
+        blocked = 0
+        deleted = 0
+        unsuccessful = 0
+        
+        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+        for chat_id in query:
+            try:
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except UserIsBlocked:
+                await del_user(chat_id)
+                blocked += 1
+            except InputUserDeactivated:
+                await del_user(chat_id)
+                deleted += 1
+            except:
+                unsuccessful += 1
+                pass
+            total += 1
+        
+        status = f"""<b><u>Broadcast Completed</u>
+
+Total Users: <code>{total}</code>
+Successful: <code>{successful}</code>
+Blocked Users: <code>{blocked}</code>
+Deleted Accounts: <code>{deleted}</code>
+Unsuccessful: <code>{unsuccessful}</code></b>"""
+        
+        return await pls_wait.edit(status)
+
+    else:
+        msg = await message.reply(REPLY_ERROR)
+        await asyncio.sleep(8)
+        await msg.delete()
+
+@Bot.on_message(filters.private & filters.command('addpaid') & filters.user(ADMINS))
+async def add_premium_user(client: Client, msg: Message):
+    if len(msg.command) != 3:
+        await msg.reply_text("usage: /addpremium user_id time_limit_months")
+        return
+    try:
+        user_id = int(msg.command[1])
+        time_limit_months = int(msg.command[2])
+        await add_premium(user_id, time_limit_months)
+        await msg.reply_text(f"User {user_id} added as a paid user with {time_limit_months}-days plan.")
+        await client.send_message(
+                chat_id= user_id,
+                text=f"**ᴄᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴs ɴᴏᴡ ʏᴏᴜ ᴀʀᴇ ᴜᴘɢʀᴀᴅᴇᴅ ᴛᴏ {time_limit_months} ᴅᴀʏs sᴜʙsᴄʀɪᴘᴛɪᴏɴ**",
+                parse_mode=ParseMode.MARKDOWN
+            )
+    except ValueError:
+        await msg.reply_text("Invalid user_id or time_limit. Please recheck.")
+
+@Bot.on_message(filters.private & filters.command('removepaid') & filters.user(ADMINS))
+async def pre_remove_user(client: Client, msg: Message):
+    if len(msg.command) != 2:
+        await msg.reply_text("useage: /removeuser user_id ")
+        return
+    try:
+        user_id = int(msg.command[1])
+        await remove_premium(user_id)
+        await msg.reply_text(f"User {user_id} has been removed.")
+    except ValueError:
+        await msg.reply_text("user_id must be an integer or not available in database.")
+
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
+@Bot.on_message(filters.private & filters.command('listpaid') & filters.user(ADMINS))
+async def list_premium_users_command(client, message):
+    premium_users = collection.find({})
+    premium_user_list = ['Premium Users in database:']
+
+    for user in premium_users:
+        user_ids = user["user_id"]
+        try:
+            user_info = await client.get_users(user_ids)
+            username = user_info.username
+            first_name = user_info.first_name
+            expiration_timestamp = user["expiration_timestamp"]
+            xt = (expiration_timestamp - time.time())
+            x = round(xt / (24 * 60 * 60))
+            premium_user_list.append(f"UserID- <code>{user_ids}</code>\nUser- @{username}\nName- <code>{first_name}</code>\nExpiry- {x} days")
+        except PeerIdInvalid:
+            premium_user_list.append(f"UserID- <code>{user_ids}</code>\nUser- <code>Invalid ID</code>\nName- <code>Unknown</code>\nExpiry- <code>N/A</code>")
+        except Exception as e:
+            premium_user_list.append(f"UserID- <code>{user_ids}</code>\nUser- <code>Error: {str(e)}</code>\nName- <code>Unknown</code>\nExpiry- <code>N/A</code>")
+
+    if premium_user_list:
+        formatted_list = [f"{user}" for user in premium_user_list]
+        await message.reply_text("\n\n".join(formatted_list))
+    else:
+        await message.reply_text("I found 0 premium users in my DB")
+
+
+
