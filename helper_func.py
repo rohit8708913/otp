8aabdd5f4c7392a75cfef20a87c6bd83c40a7239
rohit8708913 <koyeb67user@gmail.com>
2024-10-24 19:23:38 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -15,35 +15,22 @@ from datetime import datetime
 from database.database import user_data, db_verify_status, db_update_verify_status
 
 async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
+    if not (FORCE_SUB_CHANNEL or FORCE_SUB_CHANNEL2):
         return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
-    except UserNotParticipant:
-        return False
-
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL2:
-        return True
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL2, user_id = user_id)
-    except UserNotParticipant:
-        return False
-
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
+    member_status = ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER
+    for channel_id in [FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2]:
+        if not channel_id:
+            continue
+        try:
+            member = await client.get_chat_member(chat_id=channel_id, user_id=user_id)
+        except UserNotParticipant:
+            return False
+        if member.status not in member_status:
+            return False
+    return True
 
 async def encode(string):
     string_bytes = string.encode("ascii")
