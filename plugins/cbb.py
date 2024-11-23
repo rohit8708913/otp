9eeb1b9ce4d75de24d145681a400777558c56bda
rohit8708913 <koyeb67user@gmail.com>
2024-11-23 12:56:45 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,24 +1,23 @@
-#(©)Codexbotz
-
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from config import PRICE1, PRICE2, PRICE3, PRICE4, PRICE5, UPI_ID, UPI_IMAGE_URL, SCREENSHOT_URL, QR_PIC
 
-
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Rohit</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+            text=(
+                f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Rohit</a>\n"
+                f"○ Language : <code>Python3</code>\n"
+                f"○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>"
+            ),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
+                    [InlineKeyboardButton("🔒 Close", callback_data="close")]
                 ]
             )
         )
@@ -28,19 +27,30 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.reply_to_message.delete()
         except:
             pass
-
     elif data == "buy_prem":
         await query.message.edit_photo(
             photo=QR_PIC,
-            caption=f"👋 {query.from_user.username}\n\n🎖️ Available Plans :\n\n● {PRICE1}  For 0 Days Prime Membership\n\n● {PRICE2}  For 1 Month Prime Membership\n\n● {PRICE3}  For 3 Months Prime Membership\n\n● {PRICE4}  For 6 Months Prime Membership\n\n● {PRICE5}  For 1 Year Prime Membership\n\n\n💵 UPI ID -  <code>{UPI_ID}</code>\n\n\n📸 QR - ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ ({UPI_IMAGE_URL})\n\n♻️ If payment is not getting sent on above given QR code then inform admin, he will give you new QR code\n\n\n‼️ Must Send Screenshot after payment",
-            reply_markup = InlineKeyboardMarkup(
-                [   
+            caption=(
+                f"👋 {query.from_user.username}\n\n"
+                f"🎖️ Available Plans :\n\n"
+                f"● {PRICE1}  For 0 Days Prime Membership\n\n"
+                f"● {PRICE2}  For 1 Month Prime Membership\n\n"
+                f"● {PRICE3}  For 3 Months Prime Membership\n\n"
+                f"● {PRICE4}  For 6 Months Prime Membership\n\n"
+                f"● {PRICE5}  For 1 Year Prime Membership\n\n\n"
+                f"💵 UPI ID -  <code>{UPI_ID}</code>\n\n\n"
+                f"📸 QR - ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ ({UPI_IMAGE_URL})\n\n"
+                f"♻️ If payment is not getting sent on above given QR code then inform admin, he will give you new QR code\n\n\n"
+                f"‼️ Must Send Screenshot after payment"
+            ),
+            reply_markup=InlineKeyboardMarkup(
+                [
                     [
-                        InlineKeyboardButton("Send Payment Screenshot(ADMIN) 📸", url=(SCREENSHOT_URL))
+                        InlineKeyboardButton(
+                            "Send Payment Screenshot(ADMIN) 📸", url=(SCREENSHOT_URL)
+                        )
                     ],
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
+                    [InlineKeyboardButton("🔒 Close", callback_data="close")],
                 ]
             )
         )
