70cd55fc92025190a61b49efb5543c692eff6809
rohit8708913 <koyeb67user@gmail.com>
2024-12-12 13:01:51 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -63,44 +63,48 @@ async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
 
+
+
+# Add premium user with specified months
 async def add_premium(user_id, time_limit_months):
     expiration_timestamp = int(time.time()) + time_limit_months * 30 * 24 * 60 * 60
     premium_data = {
         "user_id": user_id,
         "expiration_timestamp": expiration_timestamp,
     }
-    await collection.insert_one(premium_data)
+    await collection.update_one(
+        {"user_id": user_id},
+        {"$set": premium_data},
+        upsert=True  # Ensure it updates or inserts if not present
+    )
 
-
+# Remove premium user by user_id
 async def remove_premium(user_id):
-    result = collection.delete_one({"user_id": user_id})
-    dbclient.close()
+    await collection.delete_one({"user_id": user_id})
 
+# Remove all expired users from the database
 async def remove_expired_users():
     current_timestamp = int(time.time())
+    await collection.delete_many({"expiration_timestamp": {"$lte": current_timestamp}})
 
-    # Find and delete expired users
-    expired_users = collection.find({"expiration_timestamp": {"$lte": current_timestamp}})
-    
-    for expired_user in expired_users:
-        user_id = expired_user["user_id"]
-        collection.delete_one({"user_id": user_id})
-
-    dbclient.close()
-
+# List all premium users in the database
 async def list_premium_users():
-
     premium_users = collection.find({})
-    
     premium_user_list = []
 
-    for user in premium_users:
+    async for user in premium_users:
         user_id = user["user_id"]
-        user_info = Bot.get_users(user_id)
-        username = user_info.username if user_info.username else user_info.first_name
         expiration_timestamp = user["expiration_timestamp"]
-        premium_user_list.append(f"{user_id} - {username} - Expiration Timestamp: {expiration_timestamp}")
+
+        # Calculate remaining time
+        remaining_seconds = expiration_timestamp - time.time()
+        if remaining_seconds > 0:
+            remaining_days = round(remaining_seconds / (24 * 60 * 60))
+            expiry_info = f"{remaining_days} days left"
+        else:
+            expiry_info = "Expired"
+
+        premium_user_list.append(f"UserID: {user_id} - Expiry: {expiry_info}")
 
     return premium_user_list
-
 
