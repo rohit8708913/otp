d271037a75ec2af87458cf9f1b136d889756c7e8
rohit8708913 <koyeb67user@gmail.com>
2025-03-08 19:01:07 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,67 +1,56 @@
-#Don't remove This Line From Here. Tg: @im_piro | @PiroHackz
-
-
-
 import time
-import asyncio
+import logging
+from datetime import datetime, timedelta
+import motor, asyncio
 import pymongo, os
 from config import DB_URI, DB_NAME
-from bot import Bot
-from datetime import datetime, timedelta
-from asyncio import sleep
+import motor.motor_asyncio  # Import the correct module
+
+logging.basicConfig(level=logging.INFO)
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 
+class Rohit:
 
-user_data = database['users']
-collection = database['premium-users']
+    def __init__(self, DB_URI, DB_NAME):
+        self.dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
+        self.database = self.dbclient[DB_NAME]
+        self.user_data = self.database['users']
+        
+    # USER MANAGEMENT
+    async def present_user(self, user_id: int):
+        found = await self.user_data.find_one({'_id': user_id})
+        return bool(found)
 
-default_verify = {
-    'is_verified': False,
-    'verified_time': 0,
-    'verify_token': "",
-    'link': ""
-}
+    async def add_user(self, user_id: int):
+        await self.user_data.insert_one({'_id': user_id})
+        return
 
-def new_user(id):
-    return {
-        '_id': id,
-        'verify_status': {
-            'is_verified': False,
-            'verified_time': "",
-            'verify_token': "",
-            'link': ""
-        }
-    }
+    async def full_userbase(self):
+        user_docs = await self.user_data.find().to_list(length=None)
+        user_ids = [doc['_id'] for doc in user_docs]
+        return user_ids
+
+    async def del_user(self, user_id: int):
+        await self.user_data.delete_one({'_id': user_id})
+        return
+
+    async def set_session(self, user_id: int, session: str):
+        """Store or update the user's session string in the database."""
+        await self.user_data.update_one(
+            {'_id': user_id},
+            {'$set': {'session': session}},
+            upsert=True
+        )
+
+    async def get_session(self, user_id: int):
+        """Retrieve the user's session string from the database."""
+        user = await self.user_data.find_one({'_id': user_id})
+        if user:
+            return user.get('session')
+        return None
 
 
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
-    return bool(found)
-
-async def add_user(user_id: int):
-    user = new_user(user_id)
-    user_data.insert_one(user)
-    return
-
-async def db_verify_status(user_id):
-    user = user_data.find_one({'_id': user_id})
-    if user:
-        return user.get('verify_status', default_verify)
-    return default_verify
-
-async def db_update_verify_status(user_id, verify):
-    user_data.update_one({'_id': user_id}, {'$set': {'verify_status': verify}})
-
-async def full_userbase():
-    user_docs = user_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
-    return user_ids
-
-async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
-    return
-
+# Initialize the database connection
+db = Rohit(DB_URI, DB_NAME)
